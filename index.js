e169ee0187007c93123fc3431cbbfb24ba804fd2
Nicole Rhoades <nrhoa001@gmail.com>
2018-01-14 19:31:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function happyHolidaysTo (string){
   return "Happy holidays, you!"
 }
 function holidayCountdown (string){
-  console.log("holiday name", "It's 20 days until Mother's Day!")
+  return ("holiday name", "It's 20 days until Mother's Day!")
 }
