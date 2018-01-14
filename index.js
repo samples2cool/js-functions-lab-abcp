777d8ab266db85160849e0fc6fe317306d4bc4e2
Nicole Rhoades <nrhoa001@gmail.com>
2018-01-14 19:21:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+function happyHolidays (string) {
+  return "Happy holidays!"
+}
+function happyHolidaysTo (string){
+  return "Happy holidays, you!"
+}
+function holidayCountDown (string){
+  console.log("holiday name", "It's 21 days until Christmas")
+}
